8793564657517399945d924f6d7f4ee0f0873672
Mukta Potharaju <mukta.potharaju@gmail.com>
2021-03-16 20:28:44 +0000

    Added location of the event

--- server.js
+++ server.js
@@ -18,8 +18,8 @@ app.use(bodyParser.json());
 // from a cloud data store
 const mockEvents = {
     events: [
-        { title: 'an event', id: 1, description: 'something really cool' },
-        { title: 'another event', id: 2, description: 'something even cooler' }      
+        { title: 'an event', id: 1, description: 'something really cool',location:'NY' },
+        { title: 'another event', id: 2, description: 'something even cooler' ,location:'NJ'}      
              ]
 };
 
@@ -51,6 +51,7 @@ app.post('/event', (req, res) => {
     const ev = { 
         title: req.body.title, 
         description: req.body.description,
+        location: req.body.location,
         id : mockEvents.events.length + 1
      }
     // add to the mock array
